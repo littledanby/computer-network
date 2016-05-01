d0a4e0657631d2cc10335cbebfa9942df80377b1
Xiaowen Li <xl2519@columbia.edu>
2016-05-01 05:32:15 -0400

    complete the table build part (the rest is deal with the change if weight on link[hops])

--- distance_vector_ptotocol/router.py
+++ distance_vector_ptotocol/router.py
@@ -19,60 +19,6 @@ class Router:
 		self.logfile = None		# log file name for this router
 		self.timer = None		# last time sending table to neighbors
 
-
-	# send updated routing table to neighbors every 5 seconds
-	def send_update(self):
-		send_table = [] # table: destination_id: weight
-		for destination in self.table:
-			min_dist = min(self.table[destination].values())
-			send_table.append([destination, min_dist])
-
-		wrap_table = jason.dumps(send_table)
-		for id in self.neighbors:
-			nb_info = id.split(':')
-			nb_addr = (nb_info[0], int(nb_info[1]))
-			self.s.sendto(json.dumps(send_table), addr)
-
-	# update the routing table accoding to the received data
-	def update_table(self, nb_data, nb_addr):
-		table = json.loads(data)
-		via_id = addr[0]+':'+addr[1]
-
-
-	# when ctrl C is invoked, the 
-	def send_kill_info(self):
-		return 0
-
-	# write log file
-	def write_log(self):
-		log = open(self.logfile, 'w')
-
-		return 0
-
-	# main part of Router class
-	def router(self, listen_port, neighbor):
-		try:
-			self.create_socket(listen_port)
-			self.load(neighbor)
-			while 1:
-				# if 5 seconds comes, send updates to neighbors
-				if time.time()-self.timer > Router.TIME_OUT:
-					self.send_update()
-					self.timer = time.time()
-				# wait for update info from neighbors
-				r_list, w_list, e_list = select.select([self.s],[],[],0)
-				if len(r_list) > 0:
-					nb_data, nb_addr = self.s.recvfrom(4096)
-					if len(nb_data) > 0:
-						self.update_table(nb_data, nb_addr)
-
-		except KeyboardInterrupt:
-			print 'Ctrl+C detected. Close', self.id
-			#### there is something has to be done!!!!####
-			self.send_kill_info()
-			self.s.close()
-			sys.exit()
-
 	# load the neighbor value from system input(sys.argv)
 	def load(self, neighbor):
 		for interface in neighbor:
@@ -82,7 +28,7 @@ class Router:
 			#neighbor_host = info[0]
 			#neighbor_port = int(inf[1])
 			#self.neighbors[neighbor_id] = (neighbor_host, neighbor_port)
-			neighbor_wt = int(inf[2])
+			neighbor_wt = int(info[2])
 			self.table[neighbor_id] = {}
 			self.table[neighbor_id][neighbor_id] = neighbor_wt
 		# add infinity to other links
@@ -112,6 +58,90 @@ class Router:
 		
 
 
+	# send updated routing table to neighbors every 5 seconds
+	def send_update(self):
+		send_table = [] # table: destination_id: weight
+		for destination in self.table:
+			min_dist = min(self.table[destination].values())
+			send_table.append([destination, min_dist])
+
+		wrap_table = json.dumps(send_table)
+		for id in self.neighbors:
+			nb_info = id.split(':')
+			nb_addr = (nb_info[0], int(nb_info[1]))
+			self.s.sendto(json.dumps(send_table), nb_addr)
+
+	# update the routing table accoding to the received data
+	def update_table(self, nb_data, nb_addr):
+		table = json.loads(nb_data)
+		via_id = nb_addr[0]+':'+str(nb_addr[1])
+		for each in table:
+			dest_id = each[0]
+			weight = each[1]
+			new_weight = min(self.table[via_id].values()) + weight
+			if dest_id not in self.table.keys():
+				self.table[dest_id] = {}
+				self.table[dest_id][via_id] = new_weight
+				for via in self.neighbors:
+					if via != via_id:
+						self.table[dest_id][via] = Router.INFINITY
+			else:
+				self.table[dest_id][via_id] = min(self.table[dest_id][via_id], new_weight)
+
+
+
+
+	# when ctrl C is invoked, the 
+	def send_kill_info(self):
+		return 0
+
+	# write log file
+	def write_log(self):
+		log = open(self.logfile, 'w')
+		localtime = time.strftime('%H:%M:%S', time.localtime())
+		first_line = 'Node ' + self.id + ' @ '+localtime + '\n\n'
+		log.write(first_line)
+		second_line = 'host'.ljust(16)+'port'.ljust(8)+'distance'.ljust(12)+'interface'.ljust(12)+'\n'
+		log.write(second_line)
+		count = 1
+		for dest in self.table:
+			addr = dest.split(':')
+			dist = min(self.table[dest].values())
+			line = addr[0].ljust(16)+addr[1].ljust(8)+str(dist).ljust(12)+str(count).ljust(12)+'\n'
+			count += 1
+			log.write(line)
+
+		log.close()
+
+
+	# main part of Router class
+	def router(self, listen_port, neighbor):
+		try:
+			self.create_socket(listen_port)
+			self.load(neighbor)
+			self.write_log()
+			while 1:
+				# if 5 seconds comes, send updates to neighbors
+				if time.time()-self.timer > Router.TIME_OUT:
+					self.send_update()
+					self.timer = time.time()
+				# wait for update info from neighbors
+				r_list, w_list, e_list = select.select([self.s],[],[],0)
+				if len(r_list) > 0:
+					nb_data, nb_addr = self.s.recvfrom(4096)
+					if len(nb_data) > 0:
+						self.update_table(nb_data, nb_addr)
+						self.write_log()
+
+		except KeyboardInterrupt:
+			print 'Ctrl+C detected. Close', self.id
+			#### there is something has to be done!!!!####
+			self.send_kill_info()
+			self.s.close()
+			sys.exit()
+
+
+
 
 def main():
 	if len(sys.argv) < 3:
@@ -119,7 +149,7 @@ def main():
 		sys.exit()
 	else:
 		print 'You can type (Ctrl+C) to kill it.'
-		port = sys.argv[1]
+		port = int(sys.argv[1])
 		neighbor = []
 		for i in range(2,len(sys.argv)):
 			neighbor.append(sys.argv[i])
